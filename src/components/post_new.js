6c7843df645cc534ae8c0beea2ddcffdd94cd759
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2018-06-07 17:21:56 +0200

    adding validation (and part of submit)

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -4,23 +4,39 @@ import { Field, reduxForm } from 'redux-form';
 class PostNew extends Component {
 
 	renderField(field) {
+
+		const { meta: {touched, error} } = field;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input
 					className="form-control"
 					type="text"
 					{...field.input}
 				/>
+				{/*text-help works along with has-danger for provide red alert*/}
+				<div className="text-help">
+					{/*this prevents from validating untouched fields*/}
+					{touched ? error : ''}
+				</div>
 			</div>
 		);
 	}
 
+	onSubmit(values) {
+		console.log(values);
+	}
+
 	render() {
+		const { handleSubmit } = this.props;
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<Field
 					label="Title"
+					// name property must be identical with errors. to map those
 					name="title"
 					component={this.renderField}
 				/>
@@ -34,11 +50,31 @@ class PostNew extends Component {
 					name="content"
 					component={this.renderField}
 				/>
+				<button type="submit" className="btn btn-primary">Submit</button>
 			</form>
 		);
 	}
 }
 
+function validate(values) {
+	const errors = {};
+
+	if (!values.title) {
+		errors.title = "Enter a title!";
+	}
+	if (!values.categories) {
+		errors.categories = "Enter some categories";
+	}
+	if (!values.content) {
+		errors.content = 'Enter some content please';
+	}
+
+	// if errors is empty, the form is fine to submit
+	// if errors has *any* properties, redux form assumes form is invalid
+	return errors;
+}
+
 export default reduxForm({
-	form: 'PostNewForm'
+	validate,
+	form: 'PostNewForm',
 })(PostNew);
